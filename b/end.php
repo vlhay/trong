80f10f4b62cd8b6641bbdfb4f5be5d94da62d202
vlhay <bloggernamnguyen@gmail.com>
2018-12-08 14:58:41 +0700

    b

---
+++ b/end.php
@@ -0,0 +1,7 @@
+<div class="header"><b><a href="/">Trang Chủ</a>| <a href="/">Danh Sách Tiện Ích</a></div><div class="item"><center><b style="color:green">© 2016 - Wap Tiện Ích <?php
+include "view.php";?></b><br/><b style="color:orange">Design By ADS</b></strong></center></div>
+</body></html>
+<style type='text/css'>#bttop{position:fixed;bottom:5px;right:10px;z-index:9999999999;}</style>
+<div id='bttop'><a><img src="http://i.imgur.com/SykcvFs.png" /></a></div>
+<script src='http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js' type='text/javascript'></script>
+<script type='text/javascript'>$(function(){$(window).scroll(function(){if($(this).scrollTop()!=0){$('#bttop').fadeIn();}else{$('#bttop').fadeOut();}});$('#bttop').click(function(){$('body,html').animate({scrollTop:0},800);});});</script>
---
+++ b/menu.php
@@ -0,0 +1,6 @@
+<div class="item"><a href="/tien-ich/code.php"><img src="/images/next.png"/> Xem Mã Nguồn</a></div>
+<div class="item"><a href="/tien-ich/code2.php"><img src="/images/next.png"/> Xem Mã Nguồn Part 2</a></div>
+<div class="item"><a href="/tien-ich/van-ban.php"><img src="/images/next.png"/> Sửa văn bản online (tìm văn bản , từ ngữ , kí tự,... rồi thay thế chúng hoặc xóa bỏ)</a></div>
+<div class="item"><a href="/tien-ich/css.php"><img src="/images/next.png"/> Sửa css (Sửa css theo nhiều khung nhỏ)( hỗ trợ cho máy max 5000 kí tự)</a></div>
+<div class="item"><a href="/tien-ich/ma-hoa.php"><img src="/images/next.png"/> Mã Hóa md5/sha1 </a></div>
+<div class="item"><a href="/tien-ich/chuyen.php"><img src="/images/next.png"/> Chuyển đổi văn bản HTML sang TEXT và ngược lại </a></div>
