99233c04e5da14af8266c1d4d8bd9806dba1e454
vlhay <bloggernamnguyen@gmail.com>
2018-12-08 14:59:19 +0700

    Create t.php

---
+++ t.php
@@ -0,0 +1,14 @@
+<?php 
+
+// Mệnh đề if này không được thực hiện
+// biến $sologan ko tồn tại
+$website = 'freetuts.net';
+
+    $sologan = 'Đây không phải là website freetuts.net';
+
+  
+// Nên đoạn code này sai
+$sologan .= ' vui lòng ghi rõ nguồn khi public nội dung này ở website khác';
+
+ 
+?>
